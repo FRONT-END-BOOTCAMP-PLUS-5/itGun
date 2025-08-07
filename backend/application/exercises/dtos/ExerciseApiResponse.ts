48f887fc1a5cf8605430406551c28b30ff71ebbd
zu0p <zu0p@kakao.com>
2025-08-07 15:44:15 +0900

    feat: #26 exerciseDB api (외부 API)호출을 위한 api 개발

---
+++ backend/application/exercises/dtos/ExerciseApiResponse.ts
@@ -0,0 +1,21 @@
+export interface ExerciseApiResponse {
+  success: boolean
+  meta: {
+    total: number
+    hasNextPage: boolean
+    hasPreviousPage: boolean
+    nextCursor: string
+  }
+  data: ExerciseApiData[]
+}
+
+export interface ExerciseApiData {
+  exerciseId: string
+  name: string
+  imageUrl: string
+  videoUrl: string
+  bodyParts: string[]
+  equipments: string[]
+  instructions: string[]
+  exerciseTips: string[]
+}
