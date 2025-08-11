0a3d5c9d2bc60275a59510b008757a1ea8157d81
Jiwoo Kim <jjew0504@naver.com>
2025-08-11 22:29:40 +0900

    feat: #41 공통 Text 컴포넌트 개발 및 Storybook 적용

--- ds/components/atoms/text/Text.stories.tsx
+++ ds/components/atoms/text/Text.stories.tsx
@@ -1,7 +1,6 @@
 import type { Meta, StoryObj } from "@storybook/nextjs"
-
-import { fn } from "storybook/test"
 import Text from "./Text"
+import { B1, B2, C1, C2, Display, H1, H2, S1, S2 } from "./TextWrapper"
 
 const meta = {
   title: "Components/Atoms/Text",
@@ -10,17 +9,97 @@ const meta = {
     layout: "centered",
   },
   tags: ["autodocs"],
-  argTypes: {
-    onClick: fn(),
-  },
+  argTypes: {},
 } satisfies Meta<typeof Text>
 
 export default meta
 type Story = StoryObj<typeof Text>
 
-export const Title: Story = {
+export const Default: Story = {
   render: (args) => <Text {...args} />,
   args: {
-    children: "Title Text",
+    children: "Text",
+    variant: "primary",
+  },
+}
+
+// Display
+export const DisplayText: Story = {
+  render: (args) => <Display {...args} />,
+  args: {
+    children: "Display Text",
+    variant: "primary",
+  },
+}
+
+// Headline1
+export const Headline1Text: Story = {
+  render: (args) => <H1 {...args} />,
+  args: {
+    children: "Headline1 Text",
+    variant: "primary",
+  },
+}
+
+// Headline2
+export const Headline2Text: Story = {
+  render: (args) => <H2 {...args} />,
+  args: {
+    children: "Headline2 Text",
+    variant: "primary",
+  },
+}
+
+// Subtitle1
+export const Subtitle1Text: Story = {
+  render: (args) => <S1 {...args} />,
+  args: {
+    children: "Subtitle1 Text",
+    variant: "primary",
+  },
+}
+
+// Subtitle2
+export const Subtitle2Text: Story = {
+  render: (args) => <S2 {...args} />,
+  args: {
+    children: "Subtitle2 Text",
+    variant: "primary",
+  },
+}
+
+// Body1
+export const Body1Text: Story = {
+  render: (args) => <B1 {...args} />,
+  args: {
+    children: "Body1 Text",
+    variant: "primary",
+  },
+}
+
+// Body2
+export const Body2Text: Story = {
+  render: (args) => <B2 {...args} />,
+  args: {
+    children: "Body2 Text",
+    variant: "primary",
+  },
+}
+
+// Caption1
+export const Caption1Text: Story = {
+  render: (args) => <C1 {...args} />,
+  args: {
+    children: "Caption1 Text",
+    variant: "primary",
+  },
+}
+
+// Caption2
+export const Caption2Text: Story = {
+  render: (args) => <C2 {...args} />,
+  args: {
+    children: "Caption2 Text",
+    variant: "primary",
   },
 }
--- ds/components/atoms/text/Text.tsx
+++ ds/components/atoms/text/Text.tsx
@@ -1,12 +1,26 @@
 import React from "react"
-import "../../../styles/globals.css"
+import { textVariants } from "../../../styles/text/textVariants"
+import { TextProps } from "./Text.types"
 
-interface TextProps extends React.HTMLAttributes<HTMLParagraphElement> {
-  children?: React.ReactNode
-}
+const Text: React.FC<TextProps> = ({
+  children,
+  size,
+  fontWeight,
+  variant,
+  className,
+  ...props
+}) => {
+  const variantClass = variant ? textVariants[variant] : ""
+  const sizeClass = size ?? "text-base"
+  const weightClass = fontWeight === "bold" ? "font-bold" : "font-normal"
 
-const Text: React.FC<TextProps> = ({ children, ...props }) => {
-  return <h1 {...props}>{children}</h1>
+  const combinedClassName =
+    `${variantClass} ${sizeClass} ${weightClass} ${className}`.trim()
+  return (
+    <p className={combinedClassName} {...props}>
+      {children}
+    </p>
+  )
 }
 
 export default Text
