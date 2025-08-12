a851cc3f2ee4d591d40cfbe6c81e9031d937d17c
HBDCHAEYOUNG <165043020+HBDCHAEYOUNG@users.noreply.github.com>
2025-08-12 23:08:09 +0900

    feat: #39 NextAuth 세션 관리 설정
    - 서버사이드 세션 추가 (layout.tsx)

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,11 +1,17 @@
 "use client"
 
+import { Session } from "next-auth"
 import { SessionProvider } from "next-auth/react"
 
 interface ProvidersProps {
   children: React.ReactNode
+  session: Session | null
 }
 
-export default function Providers({ children }: ProvidersProps) {
-  return <SessionProvider>{children}</SessionProvider>
+export default function Providers({ children, session }: ProvidersProps) {
+  return (
+    <SessionProvider session={session} refetchOnWindowFocus={true}>
+      {children}
+    </SessionProvider>
+  )
 }
