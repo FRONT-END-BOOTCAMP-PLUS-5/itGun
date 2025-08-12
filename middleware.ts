4930ea61f326468a3ee5883058e136d83bc60b53
HBDCHAEYOUNG <165043020+HBDCHAEYOUNG@users.noreply.github.com>
2025-08-12 17:26:14 +0900

    refactor: #39 콘솔 삭제

--- middleware.ts
+++ middleware.ts
@@ -21,24 +21,15 @@ export async function middleware(request: NextRequest) {
     req: request,
     secret: process.env.NEXTAUTH_SECRET,
   })
-  console.log("🔺미들웨어 토큰 확인", token)
 
   if (protectedRoutes.includes(request.nextUrl.pathname)) {
     if (!token) {
-      console.log(
-        "🚫 미들웨어 인증되지 않은 사용자 - 401 에러 반환",
-        Date.now()
-      )
       return NextResponse.redirect(new URL("/landing", request.url))
     }
-    console.log("✅ 미들웨어 인증된 사용자 - 접근 허용", Date.now(), "토큰:")
   }
 
   if (guestOnlyRoutes.includes(request.nextUrl.pathname)) {
     if (token) {
-      console.log(
-        "🔄 로그인된 사용자가 Guest Only 페이지 접근 - 메인으로 리다이렉트"
-      )
       return NextResponse.redirect(new URL("/", request.url))
     }
   }
