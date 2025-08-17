21d7e28f416792b933c6f9e6c8eb15bac2aa32ab
Jaeework <jaeework@gmail.com>
2025-08-17 23:45:02 +0900

    refactor: #73 트랜잭션 처리 위해 TransactionManger 작성
    - `CreateUsecase` 에 트랜잭션 처리 구현 위해 `TransactionManager` 인터페이스 및 `PrTransactionManager` 작성
    - `TransactionClient` 타입 정의

---
+++ backend/infrastructure/repositories/PrTransactionManager.ts
@@ -0,0 +1,11 @@
+import prisma from "@/utils/prisma"
+import { TransactionManager } from "@/backend/domain/repositories/TransactionManager"
+import { TransactionClient } from "@/backend/domain/common/TransactionClient"
+
+export class PrTransactionManager implements TransactionManager {
+  async executeInTransaction<T>(callback: (tx: TransactionClient) => Promise<T>): Promise<T> {
+    return await prisma.$transaction(async (tx) => {
+      return await callback(tx)
+    })
+  }
+}
