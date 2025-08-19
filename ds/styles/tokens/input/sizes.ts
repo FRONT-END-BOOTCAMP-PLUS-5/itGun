6a257d3c6ef28388907b540660d5cdd483f87c04
Jiwoo Kim <jjew0504@naver.com>
2025-08-19 10:48:30 +0900

    fix: #93 inputSize lx -> xl 오타 수정

--- ds/styles/tokens/input/sizes.ts
+++ ds/styles/tokens/input/sizes.ts
@@ -3,7 +3,7 @@ export const inputSizes = {
   sm: "w-[37px] h-[24px] text-xs",
   md: "w-[48px] h-[28px] text-xs",
   lg: "w-[54px] h-[30px] text-base",
-  lx: "w-[64px] h-[36px] text-xl",
+  xl: "w-[64px] h-[36px] text-xl",
 } as const
 
 export type InputSize = keyof typeof inputSizes
