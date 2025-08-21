3e8d7e97ae126987684d190d6e728036c65ab6fc
Jaeework <jaeework@gmail.com>
2025-08-21 09:57:30 +0900

    fix: #106 경로 오타 수정

--- app/menus/components/MenuList.tsx
+++ app/menus/components/MenuList.tsx
@@ -18,7 +18,7 @@ const MenuList = () => {
   }[] = [
     { name: "뱃지", iconName: "medal", iconColor: "secondary-yellow", pathName: "/user/badges" },
     { name: "근육 성장률", iconName: "gauge", iconColor: "secondary-purple", pathName: "/user/gauges"},
-    { name: "근로그", iconName: "calendar", iconColor: "secondary-pink", pathName: "user/logs"},
+    { name: "근로그", iconName: "calendar", iconColor: "secondary-pink", pathName: "/user/logs"},
     { name: "운동목록", iconName: "paper", iconColor: "secondary-blue", pathName: "/exercises"},
   ]
 
