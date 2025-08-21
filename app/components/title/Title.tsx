372c1d642d1adc0cfadee50ec4fcadb9c0547f2c
zu0p <zu0p@kakao.com>
2025-08-21 14:30:14 +0900

    refactor: #125 Title 공통 컴포넌트로 수정 - 그에 따라 badges 페이지에서 사용하는 Title 수정

---
+++ app/components/title/Title.tsx
@@ -0,0 +1,13 @@
+import { B2, H2 } from "@/ds/components/atoms/text/TextWrapper"
+import { TitleProps } from "./Title.type"
+
+const Title: React.FC<TitleProps> = ({ title, subTitle }) => {
+  return (
+    <div className="flex flex-col gap-[10px]">
+      <H2>{title}</H2>
+      {subTitle && <B2 variant="secondary">{subTitle}</B2>}
+    </div>
+  )
+}
+
+export default Title
