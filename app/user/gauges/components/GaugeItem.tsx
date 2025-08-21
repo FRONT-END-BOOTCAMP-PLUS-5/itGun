c995b232f7b3778be06c5d2cb04e465bd76ff5be
zu0p <zu0p@kakao.com>
2025-08-21 14:42:32 +0900

    fix: gague 값이 n.m일 때 프로그레스 바에는 0.m만 넘어가도록 %처리 추가

--- app/user/gauges/components/GaugeItem.tsx
+++ app/user/gauges/components/GaugeItem.tsx
@@ -9,7 +9,7 @@ const GaugeItem: React.FC<GaugeItemProps> = ({ label, gauge }) => {
         <S1>{BODY_PART_KO[label as keyof typeof BODY_PART_KO]}</S1>
         <B2 fontWeight="bold">{Math.floor(gauge) + 1} / 5</B2>
       </div>
-      <ProgressBar max={1} value={gauge} steps={10} />
+      <ProgressBar max={1} value={gauge % 1} steps={10} />
     </div>
   )
 }
