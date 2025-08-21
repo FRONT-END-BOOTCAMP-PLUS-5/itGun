55cf4f87a283d52181f9663a2ed49f85f7458a2a
zu0p <zu0p@kakao.com>
2025-08-21 09:46:20 +0900

    feat: #100 signin 클릭시 이동 추가, signin 텍스트 스타일 추가

--- app/landing/components/AuthFooter.tsx
+++ app/landing/components/AuthFooter.tsx
@@ -1,6 +1,6 @@
 "use client"
 import { Button } from "@/ds/components/atoms/button/Button"
-import { C1 } from "@/ds/components/atoms/text/TextWrapper"
+import { C1, S1 } from "@/ds/components/atoms/text/TextWrapper"
 import { useRouter } from "next/navigation"
 
 const AuthFooter = () => {
@@ -10,9 +10,15 @@ const AuthFooter = () => {
     router.push("/signup")
   }
 
+  const onClickSignin = () => {
+    router.push("/signin")
+  }
+
   return (
     <div className="fixed bottom-0 z-10 flex w-screen max-w-[430px] flex-col gap-[10px] bg-[var(--color-white-200)] px-[20px] py-[10px]">
-      <Button isFullWidth={true}>Sign in</Button>
+      <Button isFullWidth={true} onClick={onClickSignin}>
+        <S1>Sign in</S1>
+      </Button>
       <div className="flex items-center justify-between">
         <C1 variant="secondary">아직 회원이 아니신가요?</C1>
         <Button variant="ghost" size="xs" onClick={onClickSignup}>
