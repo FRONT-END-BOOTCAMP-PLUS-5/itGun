dffd6b1735a1aa789b7aa347bd1597ee812a2105
zu0p <zu0p@kakao.com>
2025-08-21 11:34:32 +0900

    feat: #129 user id로 body part gauge GET api 개발

---
+++ backend/application/user/gauges/usecases/GetUserGaugeUsecase.ts
@@ -0,0 +1,34 @@
+import { BodyPartGauge } from "@/backend/domain/entities/BodyPartGauge"
+import { BodyPartGaugeRepository } from "@/backend/domain/repositories/BodyPartGaugeRepository"
+import { GetUserGaugeRequestDto } from "../dtos/GetUserGaugeRequestDto"
+import { GetUserGaugeResponseDto } from "../dtos/GetUserGuageResponseDto"
+
+export class GetUserGaugeUsecase {
+  constructor(private bodyPartGaugeRepository: BodyPartGaugeRepository) {}
+
+  async execute(
+    request: GetUserGaugeRequestDto
+  ): Promise<GetUserGaugeResponseDto> {
+    try {
+      const latest: BodyPartGauge | null =
+        await this.bodyPartGaugeRepository.findLatestOneByUserId(request.userId)
+
+      if (!latest?.id) throw new Error("Body Part Gauge의 id가 존재하지 않음")
+
+      return new GetUserGaugeResponseDto(
+        latest?.id,
+        latest?.arms,
+        latest?.legs,
+        latest?.shoulders,
+        latest?.back,
+        latest?.chest,
+        latest?.core,
+        latest?.stamina,
+        latest?.earnedAt,
+        latest?.createdAt
+      )
+    } catch {
+      return new GetUserGaugeResponseDto(-1, 0, 0, 0, 0, 0, 0, 0)
+    }
+  }
+}
