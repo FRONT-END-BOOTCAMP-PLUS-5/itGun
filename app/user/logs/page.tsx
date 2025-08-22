7e17691a58dd0b1618e337b5a2c57dabd1f21eec
Jaeework <jaeework@gmail.com>
2025-08-23 03:47:21 +0900

    fix: #115 enum 타입에 맞게 소문자로 변경
    enum 타입 CalIconType에 맞게 대문자를 소문자로 수정

--- app/user/logs/page.tsx
+++ app/user/logs/page.tsx
@@ -75,9 +75,9 @@ const UserLogsPage = () => {
     iconColor: string 
   } => {
     switch (calType) {
-      case "UPPER":
+      case "upper":
         return { calTypeKo: "상체", iconName: "arm", iconColor: "secondary-yellow" }
-      case "LOWER":
+      case "lower":
         return { calTypeKo: "하체", iconName: "leg", iconColor: "secondary-blue" }
       default:
         return { calTypeKo: "유산소", iconName: "hearts", iconColor: "secondary-pink" }
