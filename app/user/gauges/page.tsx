518d3098db9c8279712f73c797ba4ac99a04274d
zu0p <zu0p@kakao.com>
2025-08-22 09:52:07 +0900

    chore: #125 title에 띄어쓰기 추가

--- app/user/gauges/page.tsx
+++ app/user/gauges/page.tsx
@@ -5,7 +5,7 @@ import GaugeList from "./components/GaugeList"
 const Gauges = () => {
   return (
     <div className="flex w-full flex-col gap-[30px]">
-      <Title title={"근육성장률"} />
+      <Title title={"근육 성장률"} />
       <MainCharacter isAnimation={false} isShadow={false} />
       <GaugeList />
     </div>
