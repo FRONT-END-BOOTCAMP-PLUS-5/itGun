e538505d05b382624e4e704bc6356f5d41d32fd9
Jaeework <jaeework@gmail.com>
2025-08-24 00:09:10 +0900

    style: #115 버튼 여백 수정

--- app/user/logs/components/LogItem.tsx
+++ app/user/logs/components/LogItem.tsx
@@ -8,39 +8,43 @@ import { useRouter } from "next/navigation"
 
 const LogItem = ({ log, calTypeMaps }: LogItemProps) => {
   const router = useRouter()
+  const logDate = new Date(log.logDate)
 
   const handleLogClick = (logId: number) => {
     router.push(`/user/logs/${logId}`)
   }
 
   return (
-    <div>
+    <div className="my-[9px]">
       <Button
         variant="ghost"
         size="xs"
         isFullWidth={true}
         onClick={() => handleLogClick(log.id)}
       >
-        <div className="grid w-full grid-cols-4 gap-4 rounded-sm border-2 border-[var(--color-primary)] px-6 py-4">
-          <div className="flex flex-col justify-center gap-1">
-            <C2>
-              {log.logDate.toLocaleDateString("en-US", { weekday: "short" })}
+        <div className="grid w-full grid-cols-4 rounded-sm border-2 border-[var(--color-primary)] px-[24px] py-[12px]">
+          <div className="flex flex-col justify-center gap-1 justify-self-start">
+            <C2 className="justify-self-center">
+              {logDate.toLocaleDateString("en-US", { weekday: "short" })}
             </C2>
-            <C1>{log.logDate.getDate()}</C1>
+            <C1 className="justify-self-center">{logDate.getDate()}</C1>
           </div>
-          <div className="justify-center flex items-center">
-            <Icon
-              name={
-                calTypeMaps(log.calIconType).iconName as keyof typeof svgList
-              }
-            />
-            <C2>{calTypeMaps(log.calIconType).calTypeKo}</C2>
+          <div className="flex col-span-2 justify-between gap-4">
+            <div className="justify-center flex items-center">
+              <Icon
+                name={
+                  calTypeMaps(log.calIconType).iconName as keyof typeof svgList
+                }
+              />
+              <C2>{calTypeMaps(log.calIconType).calTypeKo}</C2>
+            </div>
+            <div className="flex items-center justify-center">
+              <Icon name="clock" />
+              <C2>{`${log.totalDuration}min.`}</C2>
+            </div>
           </div>
-          <div className="flex items-center justify-center">
-            <Icon name="clock" />
-            <C2>{`${log.totalDuration}min.`}</C2>
-          </div>
-          <div className="flex items-center justify-center">
+          
+          <div className="flex items-center justify-center justify-self-end">
             <Icon name="rightArrow" fillColor="primary" />
           </div>
         </div>
