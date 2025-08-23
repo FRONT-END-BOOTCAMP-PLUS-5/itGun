a60302ca901eb4d1512b1178ede1a2b98042b79f
HBDCHAEYOUNG <165043020+HBDCHAEYOUNG@users.noreply.github.com>
2025-08-23 23:24:55 +0900

    style: #135 스크롤바 숨김

--- app/exercises/components/CategoryFilter.tsx
+++ app/exercises/components/CategoryFilter.tsx
@@ -4,14 +4,14 @@ import FilterButton from "./FilterButton"
 function CategoryFilter() {
   return (
     <section>
-      <div className="my-3 overflow-x-auto">
+      <div className="scrollbar-none my-3 overflow-x-auto">
         <nav className="flex min-w-max gap-2">
           {BODY_PART_MAPPINGS.map((item) => (
             <FilterButton key={item.label} item={item} filterType="bodyPart" />
           ))}
         </nav>
       </div>
-      <div className="mb-5 overflow-x-auto">
+      <div className="scrollbar-none mb-5 overflow-x-auto">
         <nav className="flex min-w-max gap-2">
           {EQUIPMENT_MAPPINGS.map((item) => (
             <FilterButton key={item.label} item={item} filterType="equipment" />
