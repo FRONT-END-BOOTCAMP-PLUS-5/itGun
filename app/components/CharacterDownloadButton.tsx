9b0b6172c22b67d027be3d8aefc5c6ad5a958caa
Jaeework <jaeework@gmail.com>
2025-08-25 23:45:51 +0900

    feat: #192 다운로드 완료 후 Toast 메세지 표출

--- app/components/CharacterDownloadButton.tsx
+++ app/components/CharacterDownloadButton.tsx
@@ -5,13 +5,14 @@ import { downloadCurrentCharacter } from "@/utils/downloadCharacter"
 import Icon from "@/ds/components/atoms/icon/Icon"
 import { Button } from "@/ds/components/atoms/button/Button"
 import { CharacterDownloadButtonProps } from "@/app/components/CharacterDownloadButton.type"
+import { useToastStore } from "@/hooks/useToastStore"
 
 const CharacterDownloadButton: React.FC<CharacterDownloadButtonProps> = ({
   userNickName,
   className = "",
 }) => {
   const [isDownloading, setIsDownloading] = useState(false)
-
+  const { showToast } = useToastStore()
   const handleDownload = async () => {
     setIsDownloading(true)
 
@@ -20,8 +21,22 @@ const CharacterDownloadButton: React.FC<CharacterDownloadButtonProps> = ({
         ? `itgun-${userNickName}-${new Date().getTime()}`
         : ""
       downloadCurrentCharacter(fileName)
+
+      showToast({
+        message: "캐릭터 이미지가 \n다운로드되었습니다!",
+        variant: "success",
+        position: "top",
+        duration: 3000,
+      })
     } catch (error) {
-      console.error("다운로드 중 오류가 발생했습니다:", error)
+      showToast({
+        message:
+          error instanceof Error
+            ? error.message
+            : "다운로드 중 오류가 발생했습니다.",
+        variant: "error",
+        position: "top",
+      })
     } finally {
       setTimeout(() => {
         setIsDownloading(false)
