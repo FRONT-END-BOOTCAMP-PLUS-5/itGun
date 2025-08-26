9bb54d87fa5ad75420c7fce0b8a2e9bfb50f20b0
Jiwoo Kim <jjew0504@naver.com>
2025-08-27 00:06:54 +0900

    chore: #230 랜딩페이지에 보여질 이미지 추가

--- app/landing/components/IntroCarousel.tsx
+++ app/landing/components/IntroCarousel.tsx
@@ -1,11 +1,31 @@
 import { Card } from "@/ds/components/atoms/card/Card"
 import { Carousel } from "@/ds/components/molecules/carousel/Carousel"
-import mainImage from "@/static/images/main.png"
+import itgun1 from "@/static/images/itgun1.png"
+import itgun2 from "@/static/images/itgun2.png"
+import itgun3 from "@/static/images/itgun3.png"
+import itgun4 from "@/static/images/itgun4.png"
+import itgun5 from "@/static/images/itgun5.png"
 
 const IntroCarousel = () => {
   const imageSet: { src: string; alt?: string }[] = [
     {
-      src: mainImage.src,
+      src: itgun1.src,
+      alt: "메인 페이지 설명 이미지",
+    },
+    {
+      src: itgun2.src,
+      alt: "메인 페이지 설명 이미지",
+    },
+    {
+      src: itgun3.src,
+      alt: "메인 페이지 설명 이미지",
+    },
+    {
+      src: itgun4.src,
+      alt: "메인 페이지 설명 이미지",
+    },
+    {
+      src: itgun5.src,
       alt: "메인 페이지 설명 이미지",
     },
   ]
