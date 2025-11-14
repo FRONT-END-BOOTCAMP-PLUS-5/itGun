9408778d82f01a182358e7426f7b18aac154f61a
zu0p <zu0p@kakao.com>
2025-11-14 14:00:57 +0900

    refactor: #284 menu 페이지 로그인하지 않은 유저에 대한 처리

--- app/menus/components/MenuList.tsx
+++ app/menus/components/MenuList.tsx
@@ -6,23 +6,56 @@ import Icon from "@/ds/components/atoms/icon/Icon"
 import { Button } from "@/ds/components/atoms/button/Button"
 import { svgList } from "@/static/svgs/svgList"
 import { useRouter } from "next/navigation"
+import { useLoginGuard } from "@/hooks/useLoginGuard"
 
 const MenuList = () => {
   const route = useRouter()
+  const loginGuard = useLoginGuard()
 
   const menuMap: {
     name: string
     iconName: keyof typeof svgList
     iconColor: string
     pathName: string
+    pathGuard: boolean
   }[] = [
-    { name: "뱃지", iconName: "medal", iconColor: "secondary-yellow", pathName: "/user/badges" },
-    { name: "근육 성장률", iconName: "gauge", iconColor: "secondary-purple", pathName: "/user/gauges"},
-    { name: "근로그", iconName: "calendar", iconColor: "secondary-pink", pathName: "/user/logs"},
-    { name: "운동목록", iconName: "paper", iconColor: "secondary-blue", pathName: "/exercises"},
+    {
+      name: "뱃지",
+      iconName: "medal",
+      iconColor: "secondary-yellow",
+      pathName: "/user/badges",
+      pathGuard: true,
+    },
+    {
+      name: "근육 성장률",
+      iconName: "gauge",
+      iconColor: "secondary-purple",
+      pathName: "/user/gauges",
+      pathGuard: true,
+    },
+    {
+      name: "근로그",
+      iconName: "calendar",
+      iconColor: "secondary-pink",
+      pathName: "/user/logs",
+      pathGuard: true,
+    },
+    {
+      name: "운동목록",
+      iconName: "paper",
+      iconColor: "secondary-blue",
+      pathName: "/exercises",
+      pathGuard: false,
+    },
   ]
 
-  const handleMenuClick = (pathName: string) => {
+  const handleMenuClick = (pathName: string, pathGuard: boolean) => {
+    if (pathGuard) {
+      loginGuard(() => {
+        route.push(pathName)
+      })
+      return
+    }
     route.push(pathName)
   }
 
@@ -34,7 +67,7 @@ const MenuList = () => {
             variant="ghost"
             isFullWidth={true}
             size="xs"
-            onClick={() => handleMenuClick(menu.pathName)}
+            onClick={() => handleMenuClick(menu.pathName, menu.pathGuard)}
           >
             <div className="flex w-full items-center justify-between rounded-sm border-2 border-[var(--color-primary)] p-5">
               <S1>{menu.name}</S1>
--- app/menus/page.tsx
+++ app/menus/page.tsx
@@ -1,16 +1,20 @@
+"use client"
+
 import React from "react"
 import MenuHeader from "./components/MenuHeader"
 import MenuList from "./components/MenuList"
 import LogoutButton from "./components/LogoutButton"
+import { useSession } from "next-auth/react"
 
 const MenuPage = () => {
+  const { data: session } = useSession()
   return (
     <div className="flex size-full flex-col items-center justify-between pb-[30px]">
       <div className="w-full">
         <MenuHeader />
         <MenuList />
       </div>
-      <LogoutButton />
+      {session?.user && <LogoutButton />}
     </div>
   )
 }
