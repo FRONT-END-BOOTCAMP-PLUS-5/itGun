a04d3f7636bbf88a4ec3d093d47012c0a0bfcab8
Jiwoo Kim <jjew0504@naver.com>
2025-11-24 10:39:53 +0900

    fix: #247 exercises/[id]/page.tsx params 타입 수정

--- app/exercises/[id]/page.tsx
+++ app/exercises/[id]/page.tsx
@@ -1,6 +1,10 @@
 import ExerciseInfo from "./ExerciseInfo"
 
-const ExerciseDetailPage = async ({ params }: { params: { id: string } }) => {
+const ExerciseDetailPage = async ({
+  params,
+}: {
+  params: Promise<{ id: string }>
+}) => {
   const { id } = await params
 
   return <ExerciseInfo id={id} />
