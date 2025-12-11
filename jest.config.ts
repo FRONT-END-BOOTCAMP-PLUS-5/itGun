d4309aad3f44188b7f134b7b28660c2604ec6354
zu0p <zu0p@kakao.com>
2025-12-11 16:25:19 +0900

    test: #301 ui test를 위한 환경설정 - babel-jest 설치 및 설정 - jsdom을 설정하기 위해 jest-fixed-jsdom 설치

--- jest.config.ts
+++ jest.config.ts
@@ -2,13 +2,36 @@ import type { JestConfigWithTsJest } from "ts-jest"
 
 const config: JestConfigWithTsJest = {
   verbose: true,
-  preset: "ts-jest/presets/default-esm",
-  testEnvironment: "node",
+  testEnvironment: "jest-fixed-jsdom",
+  moduleFileExtensions: [
+    "ts",
+    "tsx",
+    "js",
+    "mjs",
+    "cjs",
+    "jsx",
+    "json",
+    "node",
+  ],
+  transform: {
+    "^.+\\.tsx?$": [
+      "ts-jest",
+      {
+        tsconfig: {
+          jsx: "react-jsx",
+        },
+      },
+    ],
+    "^.+\\.m?js$": "babel-jest",
+  },
+
+  transformIgnorePatterns: ["node_modules/(?!.pnpm|msw|@mswjs|until-async)"],
+
   moduleNameMapper: {
     "^@/(.*)$": "<rootDir>/$1",
   },
   setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
-  testPathIgnorePatterns: [],
+  testPathIgnorePatterns: ["<rootDir>/.next/", "<rootDir>/node_modules/"],
 }
 
 export default config
