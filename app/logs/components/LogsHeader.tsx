be0ab261a5853b8c07e98f3330d2e290042811f8
HBDCHAEYOUNG <165043020+HBDCHAEYOUNG@users.noreply.github.com>
2025-12-12 10:58:23 +0900

    fix: #302 로그 페이지에서 홈으로 이동할 때만 저장 확인 다이얼로그 표시

--- app/logs/components/LogsHeader.tsx
+++ app/logs/components/LogsHeader.tsx
@@ -9,23 +9,23 @@ const LogsHeader = () => {
   const { showDialog } = useDialogStore()
 
   const handleBack = () => {
-    showDialog({
-      message: "작성 중인 기록이 저장되지 않습니다.\n정말 뒤로 가시겠습니까?",
-      variant: "error",
-      buttons: [
-        {
-          text: "네",
-          onClick: () => {
-            if (pathname === "/logs") {
+    if (pathname === "/logs") {
+      showDialog({
+        message: "작성 중인 기록이 저장되지 않습니다.\n정말 뒤로 가시겠습니까?",
+        variant: "error",
+        buttons: [
+          {
+            text: "네",
+            onClick: () => {
               router.push("/")
-            } else {
-              router.back()
-            }
+            },
           },
-        },
-        { text: "아니요", onClick: () => {} },
-      ],
-    })
+          { text: "아니요", onClick: () => {} },
+        ],
+      })
+    } else {
+      router.back()
+    }
   }
 
   return <Header className="fixed top-0" onBack={handleBack} />
