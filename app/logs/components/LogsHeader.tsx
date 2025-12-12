dbd407f5353ed59e2859c6b0c95929b0bd891b96
HBDCHAEYOUNG <165043020+HBDCHAEYOUNG@users.noreply.github.com>
2025-12-12 10:17:34 +0900

    feat: #302 로그 페이지 뒤로가기 시 확인 다이얼로그 추가

--- app/logs/components/LogsHeader.tsx
+++ app/logs/components/LogsHeader.tsx
@@ -1,17 +1,31 @@
 "use client"
 import { Header } from "@/ds/components/molecules/header/Header"
 import { usePathname, useRouter } from "next/navigation"
+import { useDialogStore } from "@/hooks/useDialogStore"
 
 const LogsHeader = () => {
   const pathname = usePathname()
   const router = useRouter()
+  const { showDialog } = useDialogStore()
 
   const handleBack = () => {
-    if (pathname === "/logs") {
-      router.push("/")
-    } else {
-      router.back()
-    }
+    showDialog({
+      message: "작성 중인 기록이 저장되지 않습니다.\n정말 뒤로 가시겠습니까?",
+      variant: "error",
+      buttons: [
+        {
+          text: "네",
+          onClick: () => {
+            if (pathname === "/logs") {
+              router.push("/")
+            } else {
+              router.back()
+            }
+          },
+        },
+        { text: "아니요", onClick: () => {} },
+      ],
+    })
   }
 
   return <Header className="fixed top-0" onBack={handleBack} />
