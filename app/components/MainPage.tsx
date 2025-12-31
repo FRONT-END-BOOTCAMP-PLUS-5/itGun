e6fd703c7ba638d1ca23390952410d43204dddf2
zu0p <zu0p@kakao.com>
2025-12-31 18:29:30 +0900

    fix: #315 메인페이지 툴팁추가

--- app/components/MainPage.tsx
+++ app/components/MainPage.tsx
@@ -8,11 +8,14 @@ import MobileNavBar from "./MobileNavBar"
 import { useEffect, useState } from "react"
 import { burky } from "@/static/svgs/burky"
 import { createAssetsFromSvgs } from "@/utils/character"
+import { useSession } from "next-auth/react"
+import Tooltip from "@/ds/components/atoms/tooltip/Tooltip"
 
 const MainPage = () => {
   const { tear } = burky
   const [decorations] = useState(() => createAssetsFromSvgs({ tear }))
   const [hasNoRecentWorkout, setHasNoRecentWorkout] = useState(false)
+  const { data: session } = useSession()
 
   const { mutate } = useCheckUserGauge({
     onNoRecentWorkout: () => setHasNoRecentWorkout(true),
@@ -24,6 +27,12 @@ const MainPage = () => {
 
   return (
     <>
+      {!session?.user && (
+        <Tooltip
+          label="운동기록을 추가하면 캐릭터가 성장합니다!"
+          variant="success"
+        />
+      )}
       <MainCharacter decorations={hasNoRecentWorkout ? decorations : []} />
       <MainUserTitle />
       <BadgeRing />
