a7091b64f458561ebe94fd761a4ce87bd8eb4466
Jaeework <jaeework@gmail.com>
2026-01-17 00:58:35 +0900

    fix: #325 Prisma 트랜잭션 타임아웃 30초로 설정

--- backend/infrastructure/repositories/PrTransactionManager.ts
+++ backend/infrastructure/repositories/PrTransactionManager.ts
@@ -6,6 +6,9 @@ export class PrTransactionManager implements TransactionManager {
   async executeInTransaction<T>(callback: (tx: TransactionClient) => Promise<T>): Promise<T> {
     return await prisma.$transaction(async (tx) => {
       return await callback(tx)
+    }, {
+      maxWait: 10000, // 10초
+      timeout: 30000, // 30초
     })
   }
 }
