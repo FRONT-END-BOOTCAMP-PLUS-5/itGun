1243ba4ac6106b5c507b96716a0ce4cc4c1d19e8
HBDCHAEYOUNG <165043020+HBDCHAEYOUNG@users.noreply.github.com>
2026-01-28 18:56:45 +0900

    style: #314 Input 비밀번호 토글 버튼 위치/스타일 조정

--- ds/components/atoms/input/Input.tsx
+++ ds/components/atoms/input/Input.tsx
@@ -71,7 +71,7 @@ export const Input: React.FC<InputProps> = ({
           type="button"
           size="xs"
           variant="ghost"
-          className="absolute top-1/2 right-0 -translate-y-1/2"
+          className="absolute top-0 right-0"
           onClick={() => {
             setIsOpenPassword((prev) => !prev)
           }}
