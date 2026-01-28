3107fa350078640a166fd33a3855093f07efd097
HBDCHAEYOUNG <165043020+HBDCHAEYOUNG@users.noreply.github.com>
2026-01-28 16:48:23 +0900

    refactor: #314 비밀번호 표시 토글 버튼 추가

--- app/user/components/PasswordCheckModal.tsx
+++ app/user/components/PasswordCheckModal.tsx
@@ -7,11 +7,9 @@ import { Input } from "@/ds/components/atoms/input/Input"
 import React, { useState } from "react"
 import { Button } from "@/ds/components/atoms/button/Button"
 import { useCheckPassword } from "@/hooks/useCheckPassword"
-import Icon from "@/ds/components/atoms/icon/Icon"
 
 const PasswordCheckModal = ({ setIsAuth }: PasswordCheckModalProps) => {
   const [password, setPassword] = useState<string | undefined>("")
-  const [isOpenPassword, setIsOpenPassword] = useState<boolean>(false)
   const { mutate } = useCheckPassword({
     checkValid: () => setIsAuth(true),
   })
@@ -27,12 +25,12 @@ const PasswordCheckModal = ({ setIsAuth }: PasswordCheckModalProps) => {
   return (
     <div
       id="password-check-container"
-      className="relative flex h-[300px] w-full flex-col border-x-2 border-t-2 border-(--color-primary) bg-(--color-white-200) p-5"
+      className="flex h-[300px] w-full flex-col border-x-2 border-t-2 border-(--color-primary) bg-(--color-white-200) p-5"
     >
       <S1>비밀번호 확인</S1>
       <div className="relative mt-6 flex flex-col gap-[20px]">
         <Input
-          type={isOpenPassword ? "text" : "password"}
+          type="password"
           value={password}
           size={"lg"}
           className="text-primary"
@@ -42,16 +40,6 @@ const PasswordCheckModal = ({ setIsAuth }: PasswordCheckModalProps) => {
             setPassword(e.target.value)
           }}
         />
-        <Button
-          size="xs"
-          variant="ghost"
-          className="absolute top-1/2 right-0 -translate-y-1/2"
-          onClick={() => {
-            setIsOpenPassword((prev) => !prev)
-          }}
-        >
-          <Icon name={isOpenPassword ? "eyeCrossed" : "eye"} />
-        </Button>
       </div>
       <div className="mt-4 flex justify-end">
         <Button size="sm" onClick={checkPassword}>
--- ds/components/atoms/input/Input.tsx
+++ ds/components/atoms/input/Input.tsx
@@ -5,6 +5,7 @@ import { InputProps } from "./Input.types"
 import { inputSizes } from "@/ds/styles/tokens/input/sizes"
 import { C2 } from "../text/TextWrapper"
 import Icon from "../icon/Icon"
+import { Button } from "../button/Button"
 
 export const Input: React.FC<InputProps> = ({
   type = "text",
@@ -20,6 +21,7 @@ export const Input: React.FC<InputProps> = ({
 }) => {
   const [validationFeedback, setValidationFeedback] = useState<boolean[]>([])
   const [errorMessage, setErrorMessage] = useState<string>("")
+  const [isOpenPassword, setIsOpenPassword] = useState<boolean>(false)
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const value = e.target.value
@@ -34,7 +36,7 @@ export const Input: React.FC<InputProps> = ({
   }
 
   const combinedClassName = `
-    border-b-1 border-[var(--color-secondary)] outline-none font-[var(--color-secondary)]
+    border-b-1 border-[var(--color-secondary)] outline-none font-[var(--color-secondary)] relative
     ${inputSizes[size]}
     ${isFullWidth ? "w-full" : ""}
     ${readOnly ? "cursor-default" : ""}
@@ -42,15 +44,31 @@ export const Input: React.FC<InputProps> = ({
   `.trim()
 
   return (
-    <div className="flex flex-col gap-1">
+    <div className="relative flex flex-col gap-1">
       <input
-        type={type}
+        type={
+          type === "password" ? (isOpenPassword ? "text" : "password") : type
+        }
         placeholder={placeholder}
         className={combinedClassName}
         onChange={handleChange}
         readOnly={readOnly}
         {...props}
       />
+
+      {type === "password" && (
+        <Button
+          type="button"
+          size="xs"
+          variant="ghost"
+          className="absolute top-1/2 right-0 -translate-y-1/2"
+          onClick={() => {
+            setIsOpenPassword((prev) => !prev)
+          }}
+        >
+          <Icon name={isOpenPassword ? "eyeCrossed" : "eye"} />
+        </Button>
+      )}
 
       {/* Error message */}
       {errorMessage && <C2 variant="error">{errorMessage}</C2>}
@@ -59,11 +77,8 @@ export const Input: React.FC<InputProps> = ({
       {validations.length > 0 && (
         <div className="flex flex-wrap gap-5">
           {validations.map((rule, idx) => (
-            <div className="flex flex-wrap items-center gap-[5px]">
-              <C2
-                key={idx}
-                variant={validationFeedback[idx] ? "success" : "disable"}
-              >
+            <div key={idx} className="flex flex-wrap items-center gap-[5px]">
+              <C2 variant={validationFeedback[idx] ? "success" : "disable"}>
                 {rule.label}
               </C2>
               <Icon
